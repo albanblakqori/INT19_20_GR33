2e3566f9c34ad25bf211a378664cb10ee6d8399c
milotplakolli <57666352+milotplakolli@users.noreply.github.com>
2020-01-04 04:38:56 +0100

    Update phone.js

--- JS/phone.js
+++ JS/phone.js
@@ -11,3 +11,24 @@ element=document.getElementById("galaxy");
 			element.src="images/tel/samsung.jpg";
 		}
 }
+
+var d= new Date();
+console.log(d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds());
+
+function change_background(){
+	if(d.getHours()>12 && (d.getHours() < 23 && d.getMinutes() < 59)){
+		document.body.style.backgroundColor="#ffffff";
+	}else{
+		document.body.style.backgroundColor="#dadada";
+	}
+}
+
+try{
+	change_background();
+	console.log('Backgroundi u nryshua me sukses');
+}catch(TypeError){
+	console.log('Tagu body nuk eshte krijuar ende, e shtojme ne onload.');
+	window.onload=function(){
+		change_background();
+	}
+}
